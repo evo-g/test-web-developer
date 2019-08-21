773c20580c20a8839b77947f97c093c8e859e291
Evodio Garcia <evodiog@gmail.com>
2019-08-20 19:35:02 -0700

    corrects small mistakes and removes many unnecessary comments and code

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-// Q: Why are we using an object, instead of an array? Are the keys significant?
 const images = {
 	man: {
 		heading: 'Man Image',
@@ -17,110 +16,84 @@ const images = {
 	}
 };
 
-// const images = [
-//  man = {
-//    heading: 'Man Image',
-//    description: 'Image of a man.',
-//    path: '../images/man.jpg'
-//  },
-//  wizard = {
-//    heading: 'Wizard Image',
-//    description: 'Image of a wizard.',
-//    path: '../images/wizard.jpg'
-//  },
-//  beast = {
-//    heading: 'Beast Image',
-//    description: 'Image of a beast.',
-//    path: '../images/beast.jpg'
-//  }
-// ];
+const createElement = ({
+	type = 'div',
+	attributes = {},
+	children = null
+}) => {
+	const container = document.createElement(type);
 
-// const getImgs = images = images.map(({
-//  heading, description, path
-// }) => {
-//  heading,
-//  description,
-//  path
-// })
+	for (let attribute in attributes) {
+		container.setAttribute(attribute, attributes[attribute]);
+	}
 
-const createImageElement = ({ heading, description, path }) => {
-	const imageElement = document.createElement('img');
-	imageElement.setAttribute('src', path);
-	imageElement.setAttribute('alt', description);
-	imageElement.setAttribute('title', heading);
-	return imageElement;
+	if (children) {
+		container.innerHTML = children;
+	}
+	return container
+}
+
+const applyMiddleHoverElements = ({ description, heading }, imagesWrapper) => {
+	const textContainer = createElement({
+		type: 'div',
+		attributes: { class: 'middle-display' }
+	});
+
+	const h1Element = createElement({
+		type: 'h1',
+		children: heading
+	});
+
+	const h2Element = createElement({
+		type: 'h2',
+		children: description
+	});
+
+	imagesWrapper.appendChild(textContainer);
+	textContainer.appendChild(h1Element);
+	textContainer.appendChild(h2Element);
 };
 
-const createImageContainerDivElement = () => {
-	const imageContainer = document.createElement('div');
-	imageContainer.setAttribute('class', 'image-container');
-	return imageContainer
-};
-
-const createTextContainerDivElement = () => {
-	const textWrapper = document.createElement('div');
-	textWrapper.setAttribute('class', 'middle-display');
-	return textWrapper
-};
-
-const createH1Element = ({ heading }) => {
-	const h1Element = document.createElement('h1');
-	h1Element.innerHTML = heading;
-	return h1Element
-};
-
-const createH2Element = ({ description }) => {
-	const h2Element = document.createElement('h2');
-	h2Element.innerHTML = description;
-	return h2Element
-};
-
-const addElementToParent = (element, parent) => {
-	parent.appendChild(element)
-};
-
-function getAppendedElements() {
-	let newImageObj = Object.values(images).map(image => {
+const appendImages = () => {
+	Object.values(images).map(image => {
 
 		const imagesWrapper = document.getElementById('images');
-		const containerElement = createImageContainerDivElement();
-		const imageElement = createImageElement(image);
 
-		addElementToParent(containerElement, imagesWrapper);
-		addElementToParent(imageElement, containerElement);
-		getMiddleHoverElements(image, containerElement)
+		const containerElement = createElement({
+			type: 'div',
+			attributes: { class: 'image-container' }
+		});
+		const imageElement = createElement({ type: 'img', attributes: {
+			src: image.path,
+			alt: image.description,
+			title: image.heading
+		} });
+
+		imagesWrapper.appendChild(containerElement);
+		containerElement.appendChild(imageElement);
+
+		applyMiddleHoverElements(image, containerElement);
 	});
-	return newImageObj
+	return
 };
 
-function getMiddleHoverElements({ description, heading }, imagesWrapper) {
-	const textContainer = createTextContainerDivElement();
-	const h1Element = createH1Element({ heading });
-	const h2Element = createH2Element({ description });
+const toggleDropdown = () => {
+	const dropdown = document.getElementsByClassName("dropdown-button");
+	let i;
 
-	addElementToParent(textContainer, imagesWrapper);
-	addElementToParent(h1Element, textContainer);
-	addElementToParent(h2Element, textContainer);
-};
+	for (i = 0; i < dropdown.length; i++) {
+		dropdown[i].addEventListener("click", function() {
+		this.classList.toggle("active");
+		var dropdownContent = this.nextElementSibling;
+		if (dropdownContent.style.display === "block") {
+		dropdownContent.style.display = "none";
+		} else {
+		dropdownContent.style.display = "block";
+		}
+		});
+	}
+} 
 
-// function toggleDropdown() {
-// 	const dropdown = document.getElementsByClassName('dropdown-button');
-// 	let i;
-	
-// 	for (i = 0; i < dropdown.length; i++) {
-// 		dropdown[i].addEventListener('click', function () {
-// 			this.classList.toggle('active');
-// 			var dropdownContent = this.nextElementSibling;
-// 			if (dropdownContent.style.display === 'block') {
-// 				dropdownContent.style.display = 'none';
-// 			} else {
-// 				dropdownContent.style.display = 'block';
-// 			}
-// 		});
-// 	}
-// 	// a bit glitchy on page have to click button twice to get it going, needed a bit more time to correct issue. 
-//  // my first attempt at dropdown menu so got code from w3 schools
-// };
 
-document.addEventListener('load', getAppendedElements());
-// document.addEventListener('load', toggleDropdown());
+window.addEventListener('load', appendImages);
+window.addEventListener('load', toggleDropdown);
